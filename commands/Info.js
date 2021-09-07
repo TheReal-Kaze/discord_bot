16c485594371c8c33e4f2238e6859de19f1402f9
Kaze <42282286+TheReal-Kaze@users.noreply.github.com>
2021-09-07 14:55:19 +0100

    Update Info.js

--- commands/Info.js
+++ commands/Info.js
@@ -5,12 +5,12 @@ module.exports = {
        run: message => {
         message.channel.send(new Discord.MessageEmbed()
         .setTitle('Salut voilà une explication du fonctionnement du DayZ:')
-        .setDescription(``)
+        .setDescription(`Tu dois tout d'abord lire le lore pour comprendre l'histoire et apres cela en lisant les règles tu comprendra petit à petit les bases du serveur etc... le reste se fait en jouant ^^ Bonne  survie :p`)
         .setColor('RANDOM'))
  
  
         },
-     name: 'info'
+     name: 'new'
      
      }
 
