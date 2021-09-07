11652f29d3b25076f6b5f2b8f99fba8d90c8ac64
Kaze <42282286+TheReal-Kaze@users.noreply.github.com>
2021-09-07 14:37:10 +0100

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -5,7 +5,7 @@ module.exports = {
        run: message => {
         message.channel.send(new Discord.MessageEmbed()
         .setTitle('VOTEZ!!' )
-        .setDescription(`Vous pouvez votez pour le serveur chaque 2 heures ! \n  [-Clickez ici ](https://top-serveurs.net/unturned/vote/frnuclear-dayz-survival) pour votez et recupérer les recompenses`)
+        .setDescription(`**Vous pouvez votez pour le serveur chaque 2 heures !** \n  -[Clickez ici ](https://top-serveurs.net/unturned/vote/frnuclear-dayz-survival) pour votez et nous donner plus de visibilité`)
         .setColor('RANDOM'))
  
  
